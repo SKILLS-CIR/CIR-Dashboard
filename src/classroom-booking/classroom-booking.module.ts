a2175eae2188aa703ef88dd0074bfcc8417fd92c
Abhijith S <abhijithsatheesh2005@gmail.com>
2026-03-16 16:14:50 +0530

    classroom booking changes

--- src/classroom-booking/classroom-booking.module.ts
+++ src/classroom-booking/classroom-booking.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ClassroomBookingService } from './classroom-booking.service';
 import { ClassroomBookingController } from './classroom-booking.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ClassroomBookingController],
   providers: [ClassroomBookingService],
 })
